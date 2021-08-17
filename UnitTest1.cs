2b282c3be59daad5ddb82e58ec84ed80c041e4f7
johndao1005 <john.d1729@gmail.com>
2021-08-17 14:51:38 +1200

    added 1 test and documentation for the testing plan

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,10 +14,30 @@ namespace CinemaApp
         }
 
         [Fact]
-        public void AdultBefore_5()
+        public void Adult_Before_5()
         {
             decimal cost = _app.Adult_Before_5(1, "adult", "monday", 4);
             Assert.Equal(14.50M, cost);
         }
+
+        /* [Theory]
+         [InlineData(13, 5, 8)]
+         [InlineData(0, -3, 3)]
+         [InlineData(0, 0, 0)]
+        [MemberData(nameof(TestData))] option 2
+         public void Add2NumberAndCompareToResultTheory(decimal expected, params decimal[] valuesToAdd)
+         {
+             foreach (var value in valuesToAdd)
+             {
+                 _app.Add(value);
+             }
+             Assert.Equal(expected, _app.Value);
+         }
+        public static IEnumerable<object[]> TestData()//option 2
+        {
+            yield return new object[] { 15, new decimal[] { 10, 5 } };
+            yield return new object[] { 15, new decimal[] { 5, 5, 5 } };
+            yield return new object[] { -15, new decimal[] { -10, -30, 20, 5 } };
+        }*/
     }
 }

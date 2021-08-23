eceb4e66f7652708656c4edc033b955a9f11cb94
johndao1005 <john.d1729@gmail.com>
2021-08-23 18:24:37 +1200

    creating the first test case

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,38 +6,41 @@ namespace CinemaApp
 {
     public class PriceTicketTest
     {
+        //Creating an instance of TicketPriceController from CinemaApp.cs called _app
         private readonly TicketPriceController _app;
 
         public PriceTicketTest()
-        {
+        {   //Arrange
             _app = new TicketPriceController();
         }
 
-        [Fact]
-        public void Adult_Before_5()
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Adult_Before_5(decimal expectd, int ticket_quanity, string customer_type, string date, decimal time)
         {
-            decimal cost = _app.Adult_Before_5(1, "adult", "monday", 4);
-            Assert.Equal(14.50M, cost);
+            //Act
+            decimal cost = _app.Adult_Before_5(ticket_quanity, customer_type, date, time);
+            //Assert
+            Assert.Equal(expectd, cost);
         }
 
-        /* [Theory]
-         [InlineData(13, 5, 8)]
-         [InlineData(0, -3, 3)]
-         [InlineData(0, 0, 0)]
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { 14.50, 1, "adult", "thursday", 4.00 };
+        }
+
+        /*[Theory]
+        [InlineData(13, 5, 8)]
+        [InlineData(0, -3, 3)]
+        [InlineData(0, 0, 0)]
         [MemberData(nameof(TestData))] option 2
          public void Add2NumberAndCompareToResultTheory(decimal expected, params decimal[] valuesToAdd)
-         {
-             foreach (var value in valuesToAdd)
-             {
-                 _app.Add(value);
-             }
-             Assert.Equal(expected, _app.Value);
-         }
-        public static IEnumerable<object[]> TestData()//option 2
         {
-            yield return new object[] { 15, new decimal[] { 10, 5 } };
-            yield return new object[] { 15, new decimal[] { 5, 5, 5 } };
-            yield return new object[] { -15, new decimal[] { -10, -30, 20, 5 } };
+            foreach (var value in valuesToAdd)
+            {
+                _app.Add(value);
+            }
+            Assert.Equal(expected, _app.Value);
         }*/
     }
 }
